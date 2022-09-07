d9668afdb74545a6796bae6b99f3f80a3d2f0afe
cacko <alex@cacko.net>
2022-09-07 06:30:16 +0100

    mergederp

--- app/api/ontv/item/tv.py
+++ app/api/ontv/item/tv.py
@@ -7,6 +7,8 @@ from datetime import datetime, timedelta, timezone
 from marshmallow import fields
 from botyo_server.output import TextOutput, Align, Column, shorten
 from app.core.time import time_hhmm
+from itertools import chain
+from functools import reduce
 
 
 @dataclass_json(undefined=Undefined.EXCLUDE)
@@ -41,16 +43,25 @@ class Event:
     def shortenEventName(self, size=36, joiner=" vs "):
         max_team_size = floor((size - len(joiner)) / 2)
         ht = shorten(
-            self.home_team,
-            max_team_size,
-            extraSize=max_team_size - len(self.away_team)
+            self.home_team, max_team_size, extraSize=max_team_size - len(self.away_team)
         )
         at = shorten(
-            self.away_team,
-            max_team_size,
-            extraSize=max_team_size - len(self.home_team)
+            self.away_team, max_team_size, extraSize=max_team_size - len(self.home_team)
         )
         return joiner.join([ht, at])
+
+
+def to_groups(res, ev: Event):
+    if not len(res):
+        return [(ev.league_name, [ev])]
+    leagues = [l for l, _ in res]
+    try:
+        idx = leagues.index(ev.league_name)
+        res[idx][1].append(ev)
+        return res
+    except ValueError:
+        res.append((ev.league_name, [ev]))
+        return res
 
 
 class TV:
@@ -67,12 +78,14 @@ class TV:
         body = self.__request.body
         events: list[Event] = Event.schema().loads(body, many=True)
         return filter(
-            lambda x: all(
-                [not x.has_expired, x.league_id in self.LEAGUES]), events
+            lambda x: all([not x.has_expired, x.league_id in self.LEAGUES]), events
         )
 
     def render(
-        self, filt: str = "", tz: ZoneInfo = ZoneInfo("Europe/London")
+        self,
+        filt: str = "",
+        group_by_league=True,
+        tz: ZoneInfo = ZoneInfo("Europe/London"),
     ) -> str:
         events = list(
             filter(
@@ -92,25 +105,35 @@ class TV:
             ev = events[0]
             TextOutput.clean()
             TextOutput.addColumns(
-                (Column(size=5, align=Align.RIGHT),
-                 Column(align=Align.LEFT, size=37)),
+                (Column(size=5, align=Align.RIGHT), Column(align=Align.LEFT, size=37)),
                 [
                     ("event:", ev.shortenEventName()),
                     ("time:", f"{time_hhmm(ev.time, tz)} {tz}"),
                     ("league:", ev.league_name),
                     ("tv:", ev.channels[0].upper()),
-                    *[("", c.upper()) for c in ev.channels[1:]]
-                ]
+                    *[("", c.upper()) for c in ev.channels[1:]],
+                ],
             )
         else:
             TextOutput.clean()
             columns = (
-                Column(title="Time", size=6, align=Align.CENTER),
-                Column(title="Event", size=36, align=Align.LEFT),
+                Column(size=6, align=Align.CENTER),
+                Column(size=36, align=Align.LEFT),
             )
-            values = [
-                [time_hhmm(ev.time, tz), ev.shortenEventName()]
-                for ev in events
-            ]
-            TextOutput.addColumns(columns, values, with_header=True)
+            if group_by_league:
+                events = list(
+                    chain.from_iterable(
+                        [
+                            [
+                                l.upper(),
+                                *[
+                                    [time_hhmm(ev.time, tz), ev.shortenEventName()]
+                                    for ev in g
+                                ],
+                            ]
+                            for l, g in reduce(to_groups, events, [])
+                        ]
+                    )
+                )
+            TextOutput.addColumns(columns, events, with_header=True)
         return TextOutput.render()
