853c8bcfb490d7a1b61817530478a35df76ef9c5
cacko <alex@cacko.net>
2022-09-07 02:30:30 +0100

    All your codebase are belong to us.

--- app/commands/cmd_importbeats.py
+++ app/commands/cmd_importbeats.py
@@ -8,13 +8,12 @@ from pathlib import Path
 from prokopiy import Progress
 import filetype
 
+
 @click.command("importbeats", short_help="beats info")
 @click.argument("path")
-@click.option("-h", "--hoplength", default=512)
-@click.option("-t", "--tightness", default=1)
 @pass_environment
 @coro
-def cli(ctx: Environment, path, hoplength: int, tightness: int):
+def cli(ctx: Environment, path):
     queue_path = Path(path)
     processed = queue_path.parent / "processed.dat"
     queue = queue_path.read_text().split("\n")
@@ -26,10 +25,9 @@ def cli(ctx: Environment, path, hoplength: int, tightness: int):
                     pf.write(f"{f}\n".encode())
                     continue
                 try:
-                    beats = Beats(fp.as_posix(), hop_length=hoplength, tightness=tightness)
+                    beats = Beats(fp.as_posix())
                     tempo = beats.tempo
                     beats = beats.beats
-                    num_beats = len(beats)
                     column_names = ["Tempo", "Number of Beats", "Beats"]
                     pf.write(f"{f}\n".encode())
                 except Exception as e:
