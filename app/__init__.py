7719ea3cbddc322597abd8700883fedd1d0a8457
cacko <alex@cacko.net>
2022-09-10 22:53:27 +0100

    Todo!!!

--- app/__init__.py
+++ app/__init__.py
@@ -5,33 +5,9 @@ from botyo_server.server import Server
 import os
 from pathlib import Path
 from app.api.server import APIServer
-import structlog
 import logging
 import signal
 import sys
-
-structlog.configure(
-    processors=[
-        structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
-    ],
-    logger_factory=structlog.stdlib.LoggerFactory(),
-)
-
-formatter = structlog.stdlib.ProcessorFormatter(
-    processors=[
-        structlog.stdlib.ProcessorFormatter.remove_processors_meta,
-        structlog.processors.StackInfoRenderer(),
-        structlog.dev.set_exc_info,
-        structlog.dev.ConsoleRenderer(),
-    ],
-)
-
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-root_logger = logging.getLogger()
-root_logger.addHandler(handler)
-root_logger.setLevel(getattr(logging, os.environ.get("ZNAYKO_LOG_LEVEL", "INFO")))
-
 
 app = Server(Path(__file__).parent.parent)
 app.servers.append(APIServer())
