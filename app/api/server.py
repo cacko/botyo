4560c974ec846ad77c859652886e96f489bde3b5
cacko <alex@cacko.net>
2022-11-22 05:27:51 +0000

    [no message]

--- app/api/server.py
+++ app/api/server.py
@@ -91,8 +91,8 @@ class _APIServer(Server):
         return {"logo": b64}
 
     def league_schedule(self, query):
-        res = Scheduler.get_jobs()
-        return res
+        res = map(lambda j: j.id, Scheduler.get_jobs())
+        return list(res)
         data_league = Footy.competition(query)
         res = []
         for game in data_league.games:
