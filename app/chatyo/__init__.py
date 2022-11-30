f1a68c59d0eaf5b1b03659770387947f10c4aa3e
cacko <alex@cacko.net>
2022-11-30 19:12:45 +0000

    less french words

--- app/chatyo/__init__.py
+++ app/chatyo/__init__.py
@@ -32,31 +32,28 @@ def getResponse(path: str, payload: Payload) -> Response:
     )
     message = ""
     attachment = None
-    is_multipart = req.is_multipart
-    logging.warning(req.body)
-    logging.warning(req.is_multipart)
-    if is_multipart:
+    if req.is_multipart:
         cp = FileStorage.storage_path
         multipart_data = req.multipart
         for part in multipart_data.parts:
             content_type = part.headers.get(b"content-type", b"").decode()  # type: ignore
             logging.debug(f"Multipart part content-type: {content_type}")
-            if "image/png" in content_type:
-                fp = cp / f"{uuid4().hex}.png"
-                fp.write_bytes(part.content)
-                attachment = Attachment(path=fp.absolute().as_posix(),
-                                        contentType="image/png", )
-            elif "image/jpeg" in content_type:
-                fp = cp / f"{uuid4().hex}.jpg"
-                fp.write_bytes(part.content)
-                attachment = Attachment(path=fp.absolute().as_posix(),
-                                        contentType="image/jpeg", )
-            else:
-                message = part.text
+            match content_type:
+                case "image/png":
+                    fp = cp / f"{uuid4().hex}.png"
+                    fp.write_bytes(part.content)
+                    attachment = Attachment(path=fp.absolute().as_posix(),
+                                            contentType="image/png", )
+                case "image/jpeg":
+                    fp = cp / f"{uuid4().hex}.jpg"
+                    fp.write_bytes(part.content)
+                    attachment = Attachment(path=fp.absolute().as_posix(),
+                                            contentType="image/jpeg", )
+                case _:
+                    message = part.text
     else:
         msg = req.json
-        message = ""
-        if message:
+        if msg:
             message = msg.get("response", "")
     return Response(
         response=message,
