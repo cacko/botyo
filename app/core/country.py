ffce8d23feef09d3124d305d8eddaecd77c68329
cacko <alex@cacko.net>
2022-12-01 08:17:38 +0000

    magic, have no clue but it works

--- app/core/country.py
+++ app/core/country.py
@@ -2,24 +2,25 @@ from pycountry import countries
 import flag
 from dataclasses import dataclass
 
+
 @dataclass
 class Country:
     name: str
 
     @property
     def flag(self):
-        if not self.name:
+        try:
+            matches = countries.search_fuzzy(self.name)
+            return flag.flagize(f":{matches.pop(0).alpha_2}:", subregions=False)
+        except LookupError:
             return ""
-        matches = countries.search_fuzzy(self.name)
-        if not len(matches):
-            return ""
-        return flag.flagize(f":{matches.pop(0).alpha_2}:", subregions=False)
 
     @property
     def country_with_flag(self) -> str:
-        if not self.name:
-            return ""
-        matches = countries.search_fuzzy(self.name)
-        if not len(matches):
+        try:
+            matches = countries.search_fuzzy(self.name)
+            return flag.flagize(
+                f"{self.name} :{matches.pop(0).alpha_2}:", subregions=False
+            )
+        except LookupError:
             return self.name
-        return flag.flagize(f"{self.name} :{matches.pop(0).alpha_2}:", subregions=False)
