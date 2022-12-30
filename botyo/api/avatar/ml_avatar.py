89bf9a923929ee8e1e64e7151d0ee21afddf8b4d
cacko <alex@cacko.net>
2022-12-30 23:08:55 +0000

    project lead is allergic to changes...

--- botyo/api/avatar/ml_avatar.py
+++ botyo/api/avatar/ml_avatar.py
@@ -18,7 +18,7 @@ class StableDiffusionAvatar(ImageCachable):
     __gender: Optional[Gender] = None
     __race: Optional[Race] = None
     __new: bool = False
-    SIZE = (512, 512)
+    SIZE = (512, 704)
 
     def __init__(self, name: str, is_new: bool = False):
         if not name:
