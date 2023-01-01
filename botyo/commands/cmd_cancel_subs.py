aa221d70c6f07a3824abeefeddd4e97f1b53adba
cacko <alex@cacko.net>
2023-01-01 15:23:52 +0000

    just trolling the repo

--- botyo/commands/cmd_cancel_subs.py
+++ botyo/commands/cmd_cancel_subs.py
@@ -2,11 +2,14 @@ from botyo.cli import pass_environment, Environment
 import click
 from botyo.commands import coro
 from botyo.server.scheduler import Scheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
 @click.command("cancel_subs", short_help="test avatar")
 @pass_environment
 @coro
 def cli(ctx: Environment):
-    print(Scheduler.get_jobs())
-    print(Scheduler.cancel_jobs())
+    worker = BackgroundScheduler()
+    scheduler = Scheduler(worker, ctx.redis_url)
+    scheduler.start()
+    print(scheduler.get_jobs())
