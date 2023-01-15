5760d7d9c31b4bc6e70e8f93435307ee34550c44
cacko <alex@cacko.net>
2023-01-15 13:52:20 +0000

    Crap. Tonight is raid night and I am already late.

--- botyo/api/avatar/__init__.py
+++ botyo/api/avatar/__init__.py
@@ -1,4 +1,3 @@
-from botyo.api.avatar.multiavatar import Avataaar, MultiAvatar
 from botyo.api.avatar.ml_avatar import StableDiffusionAvatar
 from botyo.server.socket.connection import Context
 from botyo.server.models import Attachment, EmptyResult, RenderResult
@@ -32,35 +31,35 @@ def avatar_command(context: Context) -> RenderResult:
         return EmptyResult()
 
 
-# type: ignore
-@bp.command(method=ZMethod.AVATAR_RUSSIA, desc="generates avatar for the input")
-def avaru_command(context: Context) -> RenderResult:
-    avatar = Avataaar(context.query, "ru")
-    path = avatar.path
-    res = (
-        RenderResult(
-            method=ZMethod.AVATAR_RUSSIA,
-            attachment=Attachment(path=path.as_posix(),
-                                  contentType=avatar.contentType),
-        )
-        if path
-        else EmptyResult()
-    )
-    return res
+# # type: ignore
+# @bp.command(method=ZMethod.AVATAR_RUSSIA, desc="generates avatar for the input")
+# def avaru_command(context: Context) -> RenderResult:
+#     avatar = Avataaar(context.query, "ru")
+#     path = avatar.path
+#     res = (
+#         RenderResult(
+#             method=ZMethod.AVATAR_RUSSIA,
+#             attachment=Attachment(path=path.as_posix(),
+#                                   contentType=avatar.contentType),
+#         )
+#         if path
+#         else EmptyResult()
+#     )
+#     return res
 
 
-# type: ignore
-@bp.command(method=ZMethod.AVATAR_UKRAINE, desc="generates avatar for the input")
-def avauk_command(context: Context) -> RenderResult:
-    avatar = Avataaar(context.query, "uk")
-    path = avatar.path
-    res = (
-        RenderResult(
-            method=ZMethod.AVATAR_UKRAINE,
-            attachment=Attachment(path=path.as_posix(),
-                                  contentType=avatar.contentType),
-        )
-        if path
-        else EmptyResult()
-    )
-    return res
+# # type: ignore
+# @bp.command(method=ZMethod.AVATAR_UKRAINE, desc="generates avatar for the input")
+# def avauk_command(context: Context) -> RenderResult:
+#     avatar = Avataaar(context.query, "uk")
+#     path = avatar.path
+#     res = (
+#         RenderResult(
+#             method=ZMethod.AVATAR_UKRAINE,
+#             attachment=Attachment(path=path.as_posix(),
+#                                   contentType=avatar.contentType),
+#         )
+#         if path
+#         else EmptyResult()
+#     )
+#     return res
