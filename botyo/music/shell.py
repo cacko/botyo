6d7020eb7f2064843575239c6f67380779762217
cacko <alex@cacko.net>
2023-01-24 07:39:26 +0000

    Saint Pipeline, please give me the green light

--- botyo/music/shell.py
+++ botyo/music/shell.py
@@ -24,7 +24,7 @@ class Shell(object, metaclass=ShellMeta):
     OUTPUT_REPLACED = []
 
     def __init__(self, args: str):
-        self.args = shlex.split(args.replace("'", "\'"))
+        self.args = shlex.split(args.replace("'", "\\'"))
         self.__post_init__()
 
     def __post_init__(self):
