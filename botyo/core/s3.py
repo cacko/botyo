cde4e09e4fbecdd08eda7526ecac04277785c5fd
cacko <alex@cacko.net>
2023-02-01 18:44:41 +0000

    This bunny should be killed.

--- botyo/core/s3.py
+++ botyo/core/s3.py
@@ -2,6 +2,7 @@ import boto3
 from pathlib import Path
 from botyo.core.config import Config as app_config
 import filetype
+import logging
 
 
 class S3Meta(type):
@@ -9,6 +10,7 @@ class S3Meta(type):
         return type.__call__(cls, *args, **kwds)
 
     def upload(cls, src: Path, dst: str, skip_upload: bool = False) -> str:
+        logging.debug(f"upload {src} to {dst}")
         return cls().upload_file(src, dst, skip_upload)
 
     def src_key(cls, dst):
@@ -19,17 +21,24 @@ class S3(object, metaclass=S3Meta):
     _client: boto3.client = None
 
     def __init__(self) -> None:
-        self._client = boto3.client(service_name="s3", **app_config.s3.dict())
+        cfg = app_config.s3
+        self._client = boto3.client(
+            service_name="s3",
+            aws_access_key_id=cfg.aws_access_key_id,
+            aws_secret_access_key=cfg.aws_secret_access_key,
+            region_name=cfg.region_name,
+        )
 
     def upload_file(self, src: Path, dst, skip_upload=False) -> str:
         mime = filetype.guess_mime(src)
         key = self.__class__.src_key(dst)
         if not skip_upload:
             bucket = app_config.s3.storage_bucket_name
-            self._client.upload_file(
+            res = self._client.upload_file(
                 src,
                 bucket,
                 key,
                 ExtraArgs={"ContentType": mime, "ACL": "public-read"},
             )
+            logging.debug(res)
         return key
