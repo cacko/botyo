09370d52f7966acf3018493d84a0cef435556d8d
cacko <alex@cacko.net>
2023-03-03 00:21:58 +0000

    I will not apologize for art.

--- botyo/firebase/listener.py
+++ botyo/firebase/listener.py
@@ -1,7 +1,7 @@
 import logging
 import threading
 from corethread import StoppableThread
-from google.cloud.firestore import Client, DocumentReference
+from google.cloud.firestore import Client, DocumentReference, DocumentSnapshot
 from botyo.server.core import AppServer
 
 
@@ -31,7 +31,8 @@ class DeleteListener(StoppableThread):
         for change in changes:
             match change.type.name:
                 case 'REMOVED':
-                    logging.info(f"removed {change.document.get().to_dict()}")
+                    doc = change.document
+                    logging.info(f"removed {doc.to_dict()}")
                     delete_done.set()
 
 
