4845cacb83ad3883798c77cd3a09e9a0a1f3c3f2
cacko <alex@cacko.net>
2023-08-07 19:50:09 +0100

    Fixing Fabian's bug.

--- botyo/api/avatar/__init__.py
+++ botyo/api/avatar/__init__.py
@@ -1,79 +1,41 @@
-from botyo.api.avatar.ml_avatar import StableDiffusionAvatar
+from botyo.api.avatar.multiavatar import Avataaar
 from botyo.server.socket.connection import Context
-from botyo.server.models import Attachment, ErrorResult, RenderResult
+from botyo.server.models import Attachment, EmptyResult, RenderResult
 from botyo.server.models import ZMethod
 from botyo.server.blueprint import Blueprint
-from argparse import ArgumentParser, ArgumentError
-from corestring import split_with_quotes
-import logging
-from pydantic import BaseModel, Field
 
 bp = Blueprint("avatar")
 
 
-class AvatarArgs(BaseModel):
-    prompt: list[str]
-    regenerate: bool = Field(default=False)
-
-
-@bp.command(
-    method=ZMethod.AVATAR_AVATAR, desc="generates avatar for the input", icon="face"
-)  # type: ignore
-def avatar_command(context: Context) -> RenderResult:
-    try:
-        assert context.query
-        parser = ArgumentParser(description="Avatar arguments",
-                                add_help=False, exit_on_error=False)
-        parser.add_argument("prompt", nargs="+")
-        parser.add_argument("-n", "--regenerate", action="store_true")
-        namespace, _ = parser.parse_known_args(split_with_quotes(context.query))
-        params = AvatarArgs(**namespace.__dict__)
-        avatar = StableDiffusionAvatar(" ".join(params.prompt), params.regenerate)
-        path = avatar.path
-        assert path
-        assert path.exists()
-        return RenderResult(
-            method=ZMethod.AVATAR_AVATAR,
-            attachment=Attachment(path=path.as_posix(), contentType=avatar.contentType),
+# type: ignore
+@bp.command(method=ZMethod.AVATAR_RUSSIA, desc="generates avatar for the input")
+def avaru_command(context: Context) -> RenderResult:
+    avatar = Avataaar(context.query, "ru")
+    path = avatar.path
+    res = (
+        RenderResult(
+            method=ZMethod.AVATAR_RUSSIA,
+            attachment=Attachment(path=path.as_posix(),
+                                  contentType=avatar.contentType),
         )
-    except ArgumentError:
-        return ErrorResult(
-            method=ZMethod.AVATAR_AVATAR
+        if path
+        else EmptyResult()
+    )
+    return res
+
+
+# type: ignore
+@bp.command(method=ZMethod.AVATAR_UKRAINE, desc="generates avatar for the input")
+def avauk_command(context: Context) -> RenderResult:
+    avatar = Avataaar(context.query, "uk")
+    path = avatar.path
+    res = (
+        RenderResult(
+            method=ZMethod.AVATAR_UKRAINE,
+            attachment=Attachment(path=path.as_posix(),
+                                  contentType=avatar.contentType),
         )
-    except Exception as e:
-        logging.info(e)
-        return ErrorResult()
-
-
-# # type: ignore
-# @bp.command(method=ZMethod.AVATAR_RUSSIA, desc="generates avatar for the input")
-# def avaru_command(context: Context) -> RenderResult:
-#     avatar = Avataaar(context.query, "ru")
-#     path = avatar.path
-#     res = (
-#         RenderResult(
-#             method=ZMethod.AVATAR_RUSSIA,
-#             attachment=Attachment(path=path.as_posix(),
-#                                   contentType=avatar.contentType),
-#         )
-#         if path
-#         else EmptyResult()
-#     )
-#     return res
-
-
-# # type: ignore
-# @bp.command(method=ZMethod.AVATAR_UKRAINE, desc="generates avatar for the input")
-# def avauk_command(context: Context) -> RenderResult:
-#     avatar = Avataaar(context.query, "uk")
-#     path = avatar.path
-#     res = (
-#         RenderResult(
-#             method=ZMethod.AVATAR_UKRAINE,
-#             attachment=Attachment(path=path.as_posix(),
-#                                   contentType=avatar.contentType),
-#         )
-#         if path
-#         else EmptyResult()
-#     )
-#     return res
+        if path
+        else EmptyResult()
+    )
+    return res
---
+++ botyo/api/avatar/multiavatar.py
@@ -0,0 +1,110 @@
+from hashlib import blake2b
+from pathlib import Path
+from cachable.models import BinaryStruct
+from cairosvg import svg2png
+from multiavatar.multiavatar import multiavatar
+from botyo.core.store import ImageCachable
+from PIL import Image
+from py_avataaars import (
+    PyAvataaar,
+    AvatarEnum,
+    AvatarStyle,
+    AccessoriesType,
+    FacialHairType,
+    TopType,
+)
+from random import choice
+import logging
+from typing import Optional
+
+from botyo.demographics import Demographics, Gender
+
+
+class MultiAvatar(ImageCachable):
+
+    _struct: Optional[BinaryStruct] = None
+    _name: str
+    __id = None
+    SIZE = (200, 200)
+
+    def __init__(self, name: str):
+        if not name:
+            raise ValueError
+        self._name = name
+
+
+    def _init(self):
+        if self.isCached:
+            return
+        try:
+            avatar = multiavatar(self._name, None, None)
+            svg2png(
+                bytestring=avatar,
+                write_to=self.path.as_posix(),
+                output_height=180,
+                output_width=180,
+            )
+        except Exception:
+            pass
+
+    @property
+    def id(self):
+        if not self.__id:
+            h = blake2b(digest_size=20)
+            h.update(self._name.encode())
+            self.__id = h.hexdigest()
+        return self.__id
+
+    @property
+    def filename(self):
+        return f"{self.id}.png"
+
+
+class Avataaar(MultiAvatar):
+
+    _background: str
+
+    def __init__(self, name: str, background: str):
+        if not name:
+            raise ValueError
+        self._name = name
+        self._background = background
+
+    def _init(self):
+        if self.isCached:
+            return
+        try:
+            avatar = PyAvataaar()
+            vrs = {k: choice(list(v.__class__))
+                   for k, v in vars(avatar).items()
+                   if isinstance(v, AvatarEnum)}
+            gender = Demographics.gender(self._name)
+            if gender == Gender.M:
+                while vrs["top_type"].name.startswith("LONG"):
+                    vrs["top_type"] = choice(list(TopType))
+            else:
+                vrs["facial_hair_type"] = FacialHairType.DEFAULT
+
+            avatar = PyAvataaar(
+                **{
+                    **vrs,
+                    "style": AvatarStyle.TRANSPARENT,
+                    "accessories_type": AccessoriesType.DEFAULT,
+                }
+            )
+            avatar.render_png_file(self.path.as_posix())
+            img = Image.open(self.path.as_posix())
+            img = img.convert('RGBA')
+            img = img.resize(self.SIZE, Image.BICUBIC)
+            BG = Path(__file__).parent / f"{self._background}.png"
+            background = Image.open(BG.as_posix())
+            background = background.resize(self.SIZE, Image.BICUBIC)
+            background = background.convert('RGBA')
+            background.paste(img, (10, 10), img)
+            background.save(self.path.as_posix(), "PNG")
+        except Exception as e:
+            logging.error(e)
+
+    @property
+    def filename(self):
+        return f"{self.id}_{self._background}.png"
