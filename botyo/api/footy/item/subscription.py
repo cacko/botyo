b367fcae967d346b1f0e4f75a0cec1df81f96ac4
cacko <alex@cacko.net>
2023-08-22 23:50:33 +0100

    Working on tests (haha)

--- botyo/api/footy/item/subscription.py
+++ botyo/api/footy/item/subscription.py
@@ -444,6 +444,7 @@ class Subscription(metaclass=SubscriptionMeta):
             #     raise ValueError
             #     # return self.cancel_all()
             Player.store(content.game)
+            logging.warn(content.game)
             if any([
                     ShortGameStatus(content.game.shortStatusText) in [
                         ShortGameStatus.FINAL,
