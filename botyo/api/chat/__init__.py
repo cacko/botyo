b41c89c17b680062109aeb3a5d1579f1c041da21
cacko <alex@cacko.net>
2023-10-01 02:23:33 +0100

    restored deleted entities just to be sure

--- botyo/api/chat/__init__.py
+++ botyo/api/chat/__init__.py
@@ -23,33 +23,33 @@ def dialog_command(context: Context):
     return res
 
 
-@bp.command(
-    method=ZMethod.CHAT_YOUCOMINGTODAY,
-    matcher=ZSONMatcher.PHRASE,
-    response="you coming today",
-    subscription=True
-)  # type: ignore
-def phrase_you_coming_today(context: Context):
-    msg = context.query
-    if not msg:
-        return None
-    resp = Chat.phrase(msg)
-    res = RenderResult(message=resp.response,
-                       method=ZMethod.CHAT_YOUCOMINGTODAY, plain=True)
-    return res
+# @bp.command(
+#     method=ZMethod.CHAT_YOUCOMINGTODAY,
+#     matcher=ZSONMatcher.PHRASE,
+#     response="you coming today",
+#     subscription=True
+# )  # type: ignore
+# def phrase_you_coming_today(context: Context):
+#     msg = context.query
+#     if not msg:
+#         return None
+#     resp = Chat.phrase(msg)
+#     res = RenderResult(message=resp.response,
+#                        method=ZMethod.CHAT_YOUCOMINGTODAY, plain=True)
+#     return res
 
 
-@bp.command(
-    method=ZMethod.CHAT_MINDBLOWINGINNIT,
-    matcher=ZSONMatcher.PHRASE,
-    response="mind blowing innit",
-    subscription=True
-)  # type: ignore
-def phrase_mind_blowing(context: Context):
-    msg = context.query
-    if not msg:
-        return None
-    resp = Chat.phrase(msg)
-    res = RenderResult(message=resp.response,
-                       method=ZMethod.CHAT_MINDBLOWINGINNIT, plain=True)
-    return res
+# @bp.command(
+#     method=ZMethod.CHAT_MINDBLOWINGINNIT,
+#     matcher=ZSONMatcher.PHRASE,
+#     response="mind blowing innit",
+#     subscription=True
+# )  # type: ignore
+# def phrase_mind_blowing(context: Context):
+#     msg = context.query
+#     if not msg:
+#         return None
+#     resp = Chat.phrase(msg)
+#     res = RenderResult(message=resp.response,
+#                        method=ZMethod.CHAT_MINDBLOWINGINNIT, plain=True)
+#     return res
