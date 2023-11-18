97b9f490b49c0df0a843db04d3e92383461b7bcf
cacko <alex@cacko.net>
2023-11-18 20:17:46 +0000

    We've known each other for so long

--- botyo/text/skills.py
+++ botyo/text/skills.py
@@ -7,7 +7,7 @@ from emoji import emojize
 
 
 class EntityIcon(StrEnum):
-    TECHNICAL = ":desktop_computer"
+    TECHNICAL = ":desktop_computer:"
     BUS = ":department_store:"
     TECHNOLOGY = ":wrench:"
     SOFT = ":man_office_worker:"
@@ -38,7 +38,7 @@ def output(response: list[Any]):
     tokens = [Token(**t) for t in response]
     tokens = sorted(tokens, key=lambda t: t.entity_group)
     for col, data in groupby(tokens, key=lambda t: t.entity_group):
-        row = [f"{EntityGroup(col).title:<14}:"]
+        row = [f"{EntityGroup(col).title:>14}:"]
         row.append(", ".join(set([t.word.capitalize() for t in data])))
         TextOutput.addRows([" ".join(row)])
     return TextOutput.render()
