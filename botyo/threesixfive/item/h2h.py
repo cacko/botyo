d46b298b136974017c82fbd4a69c93d2b3dfe9ea
cacko <alex@cacko.net>
2024-01-09 01:41:55 +0000

    whatthecommit.com’s server IP address could not be found.

--- botyo/threesixfive/item/h2h.py
+++ botyo/threesixfive/item/h2h.py
@@ -42,6 +42,7 @@ class H2H(RedisCachable):
     def __fetch(self):
         try:
             req = Request(Url.h2h(self.__item.id))
+            logging.warn(Url.h2h(self.__item.id))
             json = req.json
             logging.warn(json)
             details = ResponseGame(**json)  # type: ignore
