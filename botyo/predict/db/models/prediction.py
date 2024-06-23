754b9273c60f961b834b61d759814cc711075232
cacko <alex@cacko.net>
2024-06-23 18:07:29 +0100

    I'm human

--- botyo/predict/db/models/prediction.py
+++ botyo/predict/db/models/prediction.py
@@ -7,21 +7,20 @@ from .user import User
 from .game import Game
 from peewee import CharField, TimestampField, ForeignKeyField
 
+
 class Prediction(DbModel):
     User = ForeignKeyField(User)
     Game = ForeignKeyField(Game)
     prediction = CharField()
     timestamp = TimestampField()
-    
+
     @classmethod
-    def get_or_create(cls, **kwargs) -> tuple["Game", bool]:
+    def get_or_create(cls: "Prediction", **kwargs) -> tuple["Game", bool]:
         defaults = kwargs.pop("defaults", {})
         game: Game = kwargs.get("Game")
         user: User = kwargs.get("User")
-        query = cls.select().join(Game).join(User)
-        query = query.where(
-            (Game.id_event == game.id) & (User.phone == user.phone)
-        )
+        query = cls.select().join_from(Prediction, Game).join_from(Prediction, User)
+        query = query.where((Game.id_event == game.id) & (User.phone == user.phone))
         try:
             return query.get(), False
         except cls.DoesNotExist:
@@ -34,15 +33,14 @@ class Prediction(DbModel):
                     return query.get(), False
                 except cls.DoesNotExist:
                     raise exc
-                
+
     @classmethod
     def get_in_progress(cls, **kwargs):
-        pass
-        # user: User = kwargs.get("User")
-        # query = cls.select().join(Game).join(User)
+        user: User = kwargs.get("User")
+        query = cls.select().join_from(Prediction, Game).join_from(Prediction, User)
         # query = query.where(
         #     (Game.start_time == game.id) & (User.phone == user.phone)
-        # )    
+        # )
 
     class Meta:
         database = Database.db
