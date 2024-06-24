055682d90147e4d2ba87afbc1173ff077883622d
cacko <alex@cacko.net>
2024-06-24 21:50:41 +0100

    Commit committed

--- botyo/predict/db/models/prediction.py
+++ botyo/predict/db/models/prediction.py
@@ -44,6 +44,7 @@ class DbPrediction(DbModel):
 
     @classmethod
     def on_livescore_event(cls, data: UpdateData):
+        logging.warn(data)
         game: DbGame = DbGame.get(DbGame.id_event == data.message.event_id)
         home_score, away_score = cls.goals(data.score_message)
         game.home_score = home_score
