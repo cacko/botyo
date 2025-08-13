ee9df46271dfb31017c4f7bdcd1b22f2b111c12a
cacko <alex@cacko.net>
2025-08-13 14:33:08 +0100

    some brief changes

--- botyo/api/footy/item/lineups.py
+++ botyo/api/footy/item/lineups.py
@@ -2,7 +2,6 @@ from botyo.server.output import Align, Column, TextOutput
 from botyo.threesixfive.item.lineups import Lineups as LineupsData
 import logging
 from typing import Optional
-from itertools import product
 
 
 class Lineups(LineupsData):
@@ -16,37 +15,29 @@ class Lineups(LineupsData):
         away = lineups.away
         members = {m.id: m for m in lineups.members}
         try:
-            cols = [
-                (
-                    Column(size=2, align=Align.RIGHT),
-                    Column(size=25, align=Align.LEFT),
-                ),
-                (
-                    Column(size=25, align=Align.RIGHT),
-                    Column(size=2, align=Align.LEFT),
-                ),
-            ]
+            cols = (
+                Column(size=2, align=Align.RIGHT),
+                Column(size=20, align=Align.LEFT),
+                Column(size=20, align=Align.RIGHT),
+                Column(size=2, align=Align.LEFT),
+            )
             rows = [
                 [
-                    [
-                        f"{members[h.id].jerseyNumber}",
-                        f" {members[h.id].name}",
-                    ],
-                    [
-                        f"{members[a.id].name} ",
-                        f"{members[a.id].jerseyNumber}",
-                    ],
+                    f"{members[h.id].jerseyNumber}",
+                    f" {members[h.id].name}",
+                    f"{members[a.id].name} ",
+                    f"{members[a.id].jerseyNumber}",
                 ]
                 for h, a in zip(home.lineup, away.lineup)
             ]
             assert home.team.name
             assert away.team.name
             TextOutput.addColumns(
-                [Column(size=15, align=Align.LEFT), Column(size=15, align=Align.RIGHT)],
+                [Column(size=21, align=Align.LEFT),
+                 Column(size=21, align=Align.RIGHT)],
                 [[home.team.name.upper(), away.team.name.upper()]],
             )
-            for c, r in product(cols, rows):
-                TextOutput.addColumns(list(c), r)
+            TextOutput.addColumns(list(cols), rows)
             return TextOutput.render()
         except Exception as e:
             logging.exception(e, exc_info=True)
