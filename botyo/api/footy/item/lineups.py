b68578662a3f2fbd64f2c1f727bb7561456f8d54
cacko <alex@cacko.net>
2025-08-13 14:25:21 +0100

    I expected something different.

--- botyo/api/footy/item/lineups.py
+++ botyo/api/footy/item/lineups.py
@@ -2,7 +2,7 @@ from botyo.server.output import Align, Column, TextOutput
 from botyo.threesixfive.item.lineups import Lineups as LineupsData
 import logging
 from typing import Optional
-
+from itertools import product
 
 class Lineups(LineupsData):
 
@@ -15,16 +15,19 @@ class Lineups(LineupsData):
         away = lineups.away
         members = {m.id: m for m in lineups.members}
         try:
-            cols = (
+            cols = [(
                 Column(size=2, align=Align.RIGHT),
-                Column(size=13, align=Align.LEFT),
-                Column(size=13, align=Align.RIGHT),
+                Column(size=25, align=Align.LEFT),
+            ), (
+                Column(size=25, align=Align.RIGHT),
                 Column(size=2, align=Align.LEFT),
-            )
+            )]
             rows = [
                 [
                     f"{members[h.id].jerseyNumber}",
                     f" {members[h.id].name}",
+                ],
+                [
                     f"{members[a.id].name} ",
                     f"{members[a.id].jerseyNumber}",
                 ]
@@ -37,7 +40,8 @@ class Lineups(LineupsData):
                  Column(size=15, align=Align.RIGHT)],
                 [[home.team.name.upper(), away.team.name.upper()]],
             )
-            TextOutput.addColumns(list(cols), rows)
+            for c, r in product(cols, rows):
+                TextOutput.addColumns(list(c), r)
             return TextOutput.render()
         except Exception as e:
             logging.exception(e, exc_info=True)
